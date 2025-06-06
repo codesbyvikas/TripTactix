d06c0effb4ef880794145813fb8c1970b156117a
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-06-06 23:07:49 +0530

    file name issue

--- frontend/src/pages/ItineraryDisplayPage.jsx
+++ frontend/src/pages/ItineraryDisplayPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
-import { CarouselDays } from '@/components/Carousel';
+import { CarouselDays } from '@/components/carousel';
 import {
   MapPinIcon,
   DollarSignIcon,
