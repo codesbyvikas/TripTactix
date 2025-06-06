e2511e6422cbdbec9dd9a04d40e07530e0cfc861
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-06-06 23:28:22 +0530

    cors fix

--- backend/index.js
+++ backend/index.js
@@ -16,10 +16,10 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",    
-    "http://localhost:3000",  
-    "https://trip-tactix-two.vercel.app/" 
-  ], 
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "https://trip-tactix-two.vercel.app"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
