504b5025101fb3fc86c81321abad012aa0994a9e
vikaskewat025@gmail.com <vikaskewat025@gmail.com>
2025-06-07 11:17:20 +0530

    fixed port error

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: [
-    "http://localhost:5173",
+    "http://localhost:5174",
     "http://localhost:3000",
     "https://trip-tactix-two.vercel.app"
   ],
